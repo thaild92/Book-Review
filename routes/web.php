d436401d4afc265e49d51530627345c396ef87ab
thaild92 <thai@ben-story.com>
2024-10-28 13:36:20 +0700

    Add some comment for web.php

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,12 @@ Route::get('/', function () {
 });
 
 
+// Register resource routes for the BookController with only "index" and "show" actions.
 Route::resource('books', BookController::class)
     ->only(['index', 'show'])
 ;
 
+// Register nested resource routes for the ReviewController with "create", "store", and "show" actions.
 Route::resource('books.reviews', ReviewController::class)
     ->scoped(["review" => 'id'])
     ->only(['create', 'store', 'show'])
